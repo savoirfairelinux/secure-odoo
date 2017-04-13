ecbd6c370b07b8ca0a7777ff480922f1512f971a
dufresnedavid <david.dufresne@savoirfairelinux.com>
2017-04-13 10:51:14 -0400

    Disable unit test from module sale when disable_sale_analytic is installed

---
+++ disable_sale_analytic/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2017 Savoir-faire Linux
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.addons.sale.tests.test_sale_order import TestSaleOrder
+
+del TestSaleOrder.test_cost_invoicing
