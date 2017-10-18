5504d82d57460ce62aad4a40b32174935f5cc064
David Dufresne <david.dufresne@savoirfairelinux.com>
2017-10-18 16:07:33 -0400

    [10.0] Fix licence

--- action_access_control_list/models/ir_model_access.py
+++ action_access_control_list/models/ir_model_access.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2017 Savoir-faire Linux
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/gpl).
 
 from odoo import api, models
 from odoo.addons.base.ir.ir_model import IrModelAccess as BaseIrModelAccess
